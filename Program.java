46fb65889f480a7ff0f3ae25f28dab1c5d4dd69a
Nduvho Mulaudzi <154880935+CloudyG29@users.noreply.github.com>
2025-04-12 14:18:23 +0200

    read all lines

--- Program.java
+++ Program.java
@@ -1,15 +1,68 @@
 import java.io.*;
+import java.util.*;
+
+class DataPoint {
+    int x, y, z;
+    char type;
+
+    DataPoint(int x, int y, int z, char type) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + "," + z + "," + type + ")";
+    }
+}
+
+class Enclosure {
+    int x, y, z;
+    double p;
+    char type;
+
+    Enclosure(int x, int y, int z,double p,char type) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.p = p;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + "," + z + "," + p + ","+ type + ")";
+    }
+}
+
+class Deadzone {
+    int x, y, r;
+
+    Deadzone(int x, int y, int r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + "," + r +")";
+    }
+}
 
 class Program {
 
 
-    public static void main(String[] args) {
+
         int zooX=0, zooY=0, zooZ=0; 
         int droneX=0, droneY=0, droneZ=0;  
         int maxDistance=0;    
         
-        
-        
+        List<DataPoint> storages = new ArrayList<>();
+        List<Enclosure> enclosures = new ArrayList<>();
+        List<Deadzone> deadzones = new ArrayList<>();
         try {
             BufferedReader br = new BufferedReader(
                 new 
@@ -29,6 +82,79 @@ class Program {
 
             maxDistance = Integer.parseInt(br.readLine());
 
+            String storage = br.readLine();
+
+            storage = storage.substring(1, storage.length() - 1); // chop off [ and ]
+        
+            // Split into individual tuples
+            String[] parts = storage.split("\\),\\(");
+    
+
+            for (String part : parts) {
+                part = part.replace("(", "").replace(")", "");
+                String[] elements = part.split(",");
+                
+                int x = Integer.parseInt(elements[0]);
+                int y = Integer.parseInt(elements[1]);
+                int z = Integer.parseInt(elements[2]);
+                char type = elements[3].charAt(0);
+
+                storages.add(new DataPoint(x, y, z, type));
+            } 
+
+            // Read enclosures
+
+            String enclosure = br.readLine();
+
+            enclosure = enclosure.substring(1, enclosure.length() - 1); // chop off [ and ]
+        
+            // Split into individual tuples
+            String[] encs = enclosure.split("\\),\\(");
+    
+            
+            for (String part : encs) {
+                part = part.replace("(", "").replace(")", "");
+                String[] elements = part.split(",");
+                
+                int x = Integer.parseInt(elements[0]);
+                int y = Integer.parseInt(elements[1]);
+                int z = Integer.parseInt(elements[2]);
+                double p = Double.parseDouble(elements[3]);
+                char type = elements[4].charAt(0);
+
+                enclosures.add(new Enclosure(x, y, z, p, type));
+
+                }
+
+            //DEADZONES
+
+
+                String dz = br.readLine();
+                if (dz.equals("[]")) {
+                    System.out.println("No deadzones");
+                }else
+                {
+                    
+                dz = dz.substring(1, dz.length() - 1); // chop off [ and ]
+            
+                // Split into individual tuples
+                String[] dzs = dz.split("\\),\\(");
+        
+    
+                for (String par : dzs) {
+                    par = par.replace("(", "").replace(")", "");
+                    String[] elements = par.split(",");
+                    
+                    int x = Integer.parseInt(elements[0]);
+                    int y = Integer.parseInt(elements[1]);
+                    int r = Integer.parseInt(elements[2]);
+    
+                    deadzones.add(new Deadzone(x, y, r));
+                } 
+                }
+
+             
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -42,6 +168,20 @@ class Program {
         System.out.println(droneZ);
         System.out.println();
         System.out.println(maxDistance);
+        System.out.println();
+        
+        for (DataPoint point : storages) {
+            System.out.println(point);
+        }
+        System.out.println();
+        for (Enclosure point : enclosures) {
+            System.out.println(point);
+        }
+        System.out.println();
+        for (Deadzone point : deadzones) {
+            System.out.println(point);
+        }
+
 
     }
 }
